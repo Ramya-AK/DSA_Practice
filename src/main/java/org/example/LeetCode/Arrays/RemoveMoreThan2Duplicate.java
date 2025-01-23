c04f45a1706df97e2fa19ab876472bf34ee3114e
Ramya.AK <ramya.ak.developer>
2025-01-23 06:12:09 +0530

    updated

---
+++ src/main/java/org/example/LeetCode/Arrays/RemoveMoreThan2Duplicate.java
@@ -0,0 +1,41 @@
+package org.example.LeetCode.Arrays;
+
+import java.util.Arrays;
+
+public class RemoveMoreThan2Duplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,5,6,6}; // Input array
+        int[] expectedNums = {0,1,2,3,4,5,6}; // The expected answer with correct length
+
+        int k = removeDuplicates(nums); // Calls your implementation
+
+        assert k == expectedNums.length;
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+            assert nums[i] == expectedNums[i];
+        }
+    }
+
+    static int removeDuplicates(int[] arry) {
+        int j = 1;
+        int count = 0;
+        for (int i = 1; i < arry.length; i++) {
+            if(arry[i] != arry[i - 1]) {
+                arry[j] = arry[i];
+                j++;
+                count = 0;
+            } else {
+                count = count + 1;
+
+                if(count <= 1) {
+                    arry[j] = arry[i];
+                    j++;
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(arry));
+        return j;
+
+    }
+}
